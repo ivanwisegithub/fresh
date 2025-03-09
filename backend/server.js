104190982d5c4f8ce3944a0714f9a085391ab245
princeivan <ivanmukunya6565@gmail.com>
2025-03-09 16:00:57 +0200

    set to deploy

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,8 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB Connected"))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.error("MongoDB connection error:", err));
 
 // Define Schema & Model
